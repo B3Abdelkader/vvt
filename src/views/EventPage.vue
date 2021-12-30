333d2c6b5c6ad3281234467537eb32da75636729
B3Abdelkader <benabdallah.abdelkader@outlook.com>
2021-12-30 18:38:25 +0100

    adding headlessUI

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -45,16 +45,35 @@
             </router-link>
         </div>
     </div>
+
+      <Switch
+    v-model="enabled"
+    :class="enabled ? 'bg-teal-900' : 'bg-teal-700'"
+    class="relative inline-flex items-center h-6 rounded-full w-11"
+  >
+    <span class="sr-only">Enable notifications</span>
+    <span
+      :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+      class="inline-block w-4 h-4 transform bg-white rounded-full"
+    />
+  </Switch>
 </template>
 
 <script>
+import { ref } from 'vue'
+  import { Switch } from '@headlessui/vue'
 import EventCardCp from '../components/EventCardCp.vue'
 import { watchEffect } from 'vue'
 
 export default {
     name: 'eventPage',
     props: ['page'],
-    components: { EventCardCp },
+    components: { EventCardCp, Switch },
+    setup() {
+      const enabled = ref(false)
+
+      return { enabled }
+    },
     created() {
         watchEffect(() => {
             //Si un Object reactif change watchEffect est appelé :)
