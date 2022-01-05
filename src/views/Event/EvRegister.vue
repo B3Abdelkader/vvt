1f8a85a73c224e7623ed34b66a1bbe65cb6ca87a
Abdelkader BENABDALLAH <benabdallah.abdelkader@outlook.com>
2022-01-05 01:52:39 +0100

    little bug fix

--- src/views/Event/EvRegister.vue
+++ src/views/Event/EvRegister.vue
@@ -16,7 +16,7 @@ export default {
             this.GlobalStore.flashMessage = 'You are registered '+this.event.title;
             //settimout 3000
             setTimeout(() => {
-                this.GlobalStore.flashmessage = '';
+                this.GlobalStore.flashMessage = '';
             }, 3000)
             //register vers event details
             this.$router.push({ name: 'EventDetailPage'/*, params: { id: this.event.id }*/ }); // pas de params c'est envoyé par defaut8
