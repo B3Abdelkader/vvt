068d30cb317fee11d5ae8a568f52d53e61829acc
Abdelkader BENABDALLAH <benabdallah.abdelkader@outlook.com>
2022-01-18 00:24:44 +0100

    Css refonte mineur title

--- src/components/EventCardCp.vue
+++ src/components/EventCardCp.vue
@@ -1,37 +1,4 @@
 <template>
-  <!-- Card ICI
-  <router-link :to="{ name: 'EventDetailPage', params: { id: event.id } }">
-    <div class="grid justify-items-center select-none grid-">
-      <div class="max-w-xl justify-items-center bg-white rounded-xl p-2 shadow-md m-2">
-        <div class="flex flex-col w-full">
-          <div class="flex flex-row justify-between">
-            Display Event data 
-            <span class="relative text-3xl whitespace-nowrap truncate overflow-hidden">{{
-              event.title.toUpperCase()
-            }}</span>
-          </div>
-
-          <p
-            class="inline-block top-auto text-sm my-1 text-gray-600 float-right rounded-full mx-1 px-2 bg-gray-100 hover:animate-pulse font-thin"
-          >
-            By: {{ event.creator }} / {{ event.email }} on
-          </p>
-          <span
-            class="text-sm text-blue-100 font-mono bg-gray-600 rounded-full px-2 animate-pulse"
-            >{{ event.eventDate }}</span
-          >
-
-          <span class="text-gray-400 text-sm"
-            >{{ event.message.substring(0, 300) }} ...+[{{
-              event.message.length
-            }}Char]</span
-          >
-          <p class="text-gray-500 leading-4" v-for="province in event.provinces" :key="province.id" :event="province">Provinces : {{event.province}};  </p> 
-        </div>
-      </div>
-    </div>
-  </router-link> -->
-
   <router-link :to="{ name: 'EventDetailPage', params: { id: event.id } }">
     <a
       href="/"
--- src/views/event/HomePage.vue
+++ src/views/event/HomePage.vue
@@ -1,16 +1,22 @@
 <template>
-  <h1
-    class="flex self-center max-w-lg mb-6 font-sans text-6xl select-none font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none"
+  <div
+    class="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12"
   >
-    Actual<!--<br class="hidden md:block" /> -->
+    <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+      <h1
+        class="flex self-center max-w-lg mb-6 font-sans text-6xl select-none font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none"
+      >
+        Actual<!--<br class="hidden md:block" /> -->
 
-    <span class="relative px-1">
-      <div
-        class="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400"
-      ></div>
-      <span class="relative inline-block text-deep-purple-accent-400">Events</span>
-    </span>
-  </h1>
+        <span class="relative px-1">
+          <div
+            class="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400"
+          ></div>
+          <span class="relative inline-block text-deep-purple-accent-400">Events!</span>
+        </span>
+      </h1>
+    </div>
+  </div>
   <div
     class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
   >
@@ -27,7 +33,20 @@
           :to="{ name: 'EventsPage', query: { page: page - 1 } }"
           rel="prev"
           v-if="page != 1"
-          >Previous Page
+          ><svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 19l-7-7 7-7"
+            ></path>
+          </svg>
         </router-link>
 
         <router-link
@@ -36,7 +55,20 @@
           rel="next"
           v-if="page < 7"
         >
-          Next Page
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            ></path>
+          </svg>
           <!-- Next -->
         </router-link>
       </div>
